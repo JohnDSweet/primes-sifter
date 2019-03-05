f64b7be8e5ac26709f338ee8b7456c1f29223a3a
john sweet and rakhat tutueva <rakhat@>
2019-03-04 21:02:43 -0800

    add initial files

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,27 @@
-var palCalc = function(index) {
-  var regStr = index.join();
-  var reverseStr = index.reverse().join();
-  if (regStr === reverseStr) {
-    return true;
-  } else {
-    return false;
+var primeVar = 2;
+console.log(primeNums[0]);
+var prime = function(index) {
+ var numbers = [];
+ for (i = index; i > 0; i -= 1) {
+   numbers.push(i);
+ };
+ console.log(numbers);
+  for (var i = numbers.length; i < 0; i -= 1){
+    if (i % prime === 0) {
+      numbers.slice(i);
+    }
   }
 }
+
+};
 
 $(document).ready(function() {
   $("form#pal").submit(function(event) {
     event.preventDefault();
-    var input = ($("input#pal-input").val());
-    var palInput = input.split('')
-    var result = palCalc(palInput);
+    var palInput = parseInt($("input#pal-input").val());
+    var result = prime(palInput);
 
-    $(".pal").text(input);
+    $(".pal").text(palInput);
     if (result === true) {
       $(".not").text(" ");
     } else {
